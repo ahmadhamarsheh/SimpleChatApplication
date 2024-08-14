0ebc4426aa3eb4ec40c9853c4308d9ba509cccbf
AhmadHamarsheh <ahmadhamarshe@outlook.com>
2024-08-14 03:01:30 +0300

    Add project files.

---
+++ SimpleChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SimpleChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string fromUser, string message)
+        {
+            await Clients.All.SendAsync("ReciveMessage", fromUser, message);
+        }
+    }
+}
---
+++ SimpleChatApp/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+connection.on("ReceiveMessage", function (fromUser, message) {
+    var now = new Date();
+    var formattedDate = now.toLocaleString();
+    var msg = `<strong>${fromUser}:</strong> ${message} <span class="timestamp">${formattedDate}</span>`;
+
+    var li = document.createElement("li");
+    li.className = 'list-group-item'; // Apply the same styling as in your HTML
+    li.innerHTML = msg;
+
+    $("#list").prepend(li);
+});
+
+connection.start();
+
+$("#btnSend").on("click", function () {
+    var fromUser = $("#txtUser").val();
+    var message = $("#txtMessage").val();
+
+    connection.invoke("SendMessage", fromUser, message);
+});
